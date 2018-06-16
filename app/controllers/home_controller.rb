24adb93b30adabd63005bbaa306348c7f3a2a9db
Floore <flore.wang@gmail.com>
2018-06-16 10:30:00 +0000

    added class index for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    
+    @name = "Fete de la Musique"
+    @place = "Paris"
+    @time = "21 juin 2018"
+    @artists = ["Ariana Grande", "David Guetta", "Eminem"]
   end
 end
