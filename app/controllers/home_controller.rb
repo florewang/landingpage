047ce49013d42a3da33a6b21b34883b86d248081
Floore <flore.wang@gmail.com>
2018-06-16 11:01:41 +0000

    concatenation of hashes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,30 @@ class HomeController < ApplicationController
     @name = "Fete de la Musique"
     @place = "Paris"
     @time = "21 juin 2018"
-    @artists = ["Ariana Grande", "David Guetta", "Eminem"]
+    @artists = [
+      {
+        first_name: "ariana",
+        last_name: "grande",
+        age: 30,
+        style: "pop"
+      },
+      {
+        first_name: "david",
+        last_name: "guetta",
+        age: 45,
+        style: "electro"
+      },
+      {
+        first_name: "eminem",
+        last_name: "marshall",
+        age: 42,
+        style: "rap"
+      }
+      ]
+      
+      
+      
+      
+      
   end
 end
